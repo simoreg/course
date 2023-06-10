42a0d801bd1e4b39a10eb31cdf17167084e88f5f
simoreg <leandrosimoreg@gmail.com>
2023-06-10 20:23:07 -0300

    Implementação de mapeamentos e relacionamentos JPA

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,7 +1,9 @@
 package com.ead.course.services;
 
+import com.ead.course.models.CourseModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CourseService {
+    public void delete(CourseModel courseModel);
 }
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,16 +1,45 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     private final CourseRepository courseRepository;
 
-    public CourseServiceImpl(CourseRepository courseRepository){
+    private final ModuleRepository moduleRepository;
+
+    private final LessonRepository lessonRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository,LessonRepository lessonRepository){
         this.courseRepository = courseRepository;
+        this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
     }
 
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModuleIntoCourse(courseModel.getCourseId());
+        if(!moduleModelList.isEmpty()){
+            moduleModelList.stream().forEach(module ->{
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if(!lessonModelList.isEmpty()){
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            });
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
+    }
 }
